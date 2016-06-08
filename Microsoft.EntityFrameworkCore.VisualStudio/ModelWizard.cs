15e7d5d21f99c6d72b1e5de17cf7ee627cd2c44c
Brice Lambson <bricelam@microsoft.com>
2016-06-08 12:28:36 -0700

    Add some code

---
+++ Microsoft.EntityFrameworkCore.VisualStudio/ModelWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace Microsoft.EntityFrameworkCore.VisualStudio
+{
+    internal class ModelWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(
+            object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind,
+            object[] customParams)
+        {
+            var dte = (_DTE)automationObject;
+
+            var window = new MainWindow();
+            window.ShowDialog();
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
